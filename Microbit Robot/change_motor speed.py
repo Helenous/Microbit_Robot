9b7052414e7c5684a36bfa08ef53b12c3d7e432e
Helenous <heleneviro@hotmail.com>
2020-11-29 10:21:46 +0000

    update test

--- Microbit Robot/change_motor speed.py
+++ Microbit Robot/change_motor speed.py
@@ -2,7 +2,7 @@ from microbit import *
 
 while True:
                                 #forward at 100% speed #testNov29
-        pin8.write_digital(1)
+        pin8.write_digital(0)
         pin12.write_digital(0)
         pin0.write_digital(1)
         pin16.write_digital(0) 
